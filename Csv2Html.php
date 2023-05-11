ac307e99f26d6575892beaa28e59302529666758
LuRo <lukas.rothe@bluewin.ch>
2023-05-11 22:37:43 +0200

    Add files via upload

---
+++ Csv2Html.php
@@ -0,0 +1,77 @@
+<?php
+/**
+ *	iSeek Csv to HTML
+ *
+ * 	An Automad Csv to HTML convertion extension.
+ *
+ * 	@author Lukas Rothe
+ * 	@copyright Copyright (C) 2023 Lukas Rothe - <https://iseek.ch> 
+ * 	@license MIT license
+ */
+
+ namespace Automad;
+
+ defined('AUTOMAD') or die('Direct access not permitted!');
+ 
+ 
+ class Csv2Html {
+
+    /**
+	 *  The main function.
+	 *
+	 * 	@param string $text
+     *  @param boolean $hasHeader
+	 * 	@param object $Automad
+	 * 	@return string the output of the extension
+	 */
+	
+	public function Csv2Html($text, $hasHeader, $Automad) {
+
+        $str_line_separator = "#";
+        $str_cell_separator = ";";
+
+     
+        $row = 1;
+        if (strlen($text) > 0) {
+
+            $table_rows = explode($str_line_separator, $text);
+            $htmlval = "<table class='table'>";
+            $num_rows = count($table_rows);
+
+            for ($table_row=0; $table_row < $num_rows; $table_row++){
+                $row_data = str_getcsv($text, $str_cell_separator);
+
+                /* if param hasHeader is true then create header  */
+                if ($hasHeader == true){
+                    $htmlval = $htmlval. "\n<thead>";
+                } else {
+                    $htmlval = $htmlval. "\n<tbody>";
+                }
+
+                $htmlval = $htmlval."<tr>";
+                $num_cells = count($row_data);        
+                $row++;
+                for ($table_cell=0; $table_cell < $num_cells; $table_cell++) {
+                    if($row_data[$table_cell] != "" || trim($row_data[$table_cell]) != " "){
+                        if ($hasHeader == true) {
+                            // header row
+                            $htmlval = $htmlval. "<th class='cols' align='center'>".$row_data[$table_cell]."</th>\n";
+                        } else {
+                            // table data rows
+                            $htmlval = $htmlval. "<td class='cols' align='center'>".$row_data[$table_cell]."</td>\n";
+                        }
+                    }else{
+                        $htmlval = $htmlval. "<td class='cols'>&nbsp;</td>";
+                    }
+                }
+                $htmlval = $htmlval."</tr>";
+                if ($hasHeader == true){
+                    $htmlval = $htmlval. "\n</thead>";
+                } else {
+                    $htmlval = $htmlval. "\n</tbody>";
+                }
+            }
+            $htmlval = $htmlval."</table>";
+        }
+    }
+}
